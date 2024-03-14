b0072e0c50180c87e3e2e7fd945312e130d9a96d
pratheep <npr9713@gmail.com>
2024-03-14 23:10:22 +0530

    files were updated

--- app.py
+++ app.py
@@ -42,7 +42,8 @@ def input_pdf_setup(uploaded_file):
 
 st.set_page_config(page_title="ATS Resume EXpert")
 st.header("Resume Tracker")
-input_text=st.text_area("Job Description: ",key="input")
+input_text = "Job description: "
+input_text+=st.text_area("Job Description: ",key="input")
 uploaded_file=st.file_uploader("Upload your resume in pdf format",type=["pdf"])
 
 
@@ -63,9 +64,8 @@ input_prompt1 = """
 """
 
 input_prompt3 = """
-You are an skilled ATS (Applicant Tracking System) scanner with a deep understanding of data science and ATS functionality, 
-your task is to evaluate the resume against the provided job description. give me the percentage of match if the resume matches
-the job description. First the output should come as percentage and then keywords missing and last final thoughts.
+Assume yourself as a You are an experienced Technical Human Resource Manager,your task is to review the provided resume against the job description.
+provide a percentage value which signifies the match between job description  and the resume.
 """
 
 if submit1:
