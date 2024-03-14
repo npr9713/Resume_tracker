a4927dcde8a0056c342af2e6fa07553e0e240660
pratheep <npr9713@gmail.com>
2024-03-14 23:14:45 +0530

    files were updated

--- app.py
+++ app.py
@@ -65,7 +65,7 @@ input_prompt1 = """
 
 input_prompt3 = """
 Assume yourself as a You are an experienced Technical Human Resource Manager,your task is to review the provided resume against the job description.
-provide a percentage value which signifies the match between job description  and the resume.
+provide a percentage value which signifies the match between job description  and the resume. Also specify the reason for the given percentage if the percentage is less than 75 percent tell them where they can improve to match the job description.
 """
 
 if submit1:
